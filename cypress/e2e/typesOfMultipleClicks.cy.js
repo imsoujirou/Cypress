d16d898f98bdc86b96aa3325a6fabe67cf919024
Armand <99232659+imsoujirou@users.noreply.github.com>
2024-04-11 12:35:46 +0800

    types of multiple clicks

---
+++ cypress/e2e/typesOfMultipleClicks.cy.js
@@ -0,0 +1,30 @@
+describe("Simulating clicks", ()=> {
+  it("Multiple clicks", ()=> {
+    cy.visit("https://saucedemo.com")
+    Cypress._.times(10, function () {
+      cy.get('[data-test="login-button"]').click({force:true})
+    });
+  });
+
+  it("Simple click", ()=> {
+    cy.visit("https://saucedemo.com")
+    cy.get('[data-test="login-button"]').click({force:true})
+  });
+
+  it("Multiple clicks using for loop", ()=> {
+    cy.visit("https://saucedemo.com")
+    for(let i=0;i<10;i++){
+      cy.get('[data-test="login-button"]').click({force:true})
+    i++;
+    }
+  });
+
+  it("Multiple clicks using chain commands", ()=> {
+    cy.visit("https://saucedemo.com")
+    for(let i=0;i<10;i++){
+      cy.get('[data-test="login-button"]').click({force:true})
+    i++;
+    }
+  });
+
+});
